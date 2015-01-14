c2dd2ae1ba00a985bb877ca2eecbbdbe3639314c
Luke Bacon <luke@equivalentideas.com>
2015-01-14 15:32:03 +1100

    Add script to hide and then progressively reveal donation steps
    current solution to https://github.com/openaustralia/oaf/issues/178 .

--- custom.js
+++ custom.js
@@ -1,1 +1,35 @@
-console.log("oaf-thematic's custom.js is ready to rock and/or roll");
+jQuery(function ($) {
+  // maker sure there are not messages being displayed
+  if ($(".CRM_Contribute_Form_Contribution_Main").length && $("form > .messages").length === 0) {
+    $(".crm-submit-buttons").after('<a class="oaf-donation-button oaf-donation-step-reveal" role="button" href="#donation-step-2">Next step</button>');
+
+    var btn = $(".oaf-donation-step-reveal");
+
+    if ( $(".oaf-donation-step-reveal").length > 0 ) {
+      // Card info group
+      $(".credit_card_info-group").addClass("donation-step donation-step-hidden donation-step-2").attr("id", "donation-step-2");
+      // Billing information group
+      $(".billing_name_address-group").addClass("donation-step donation-step-hidden donation-step-3").attr("id", "donation-step-3");
+      // Form submit button
+      $(".crm-submit-buttons").addClass("donation-step donation-step-hidden donation-step-4");
+
+      var step2 = $(".donation-step-2"),
+          step3 = $(".donation-step-3"),
+          step4 = $(".donation-step-4");
+
+      // reveal the next step
+      btn.click(function(event) {
+        event.preventDefault();
+        if (step2.hasClass("donation-step-hidden")) {
+          step2.removeClass("donation-step-hidden");
+          btn.attr("href", "#donation-step-3");
+          btn.text("Final step");
+        } else if (!step2.hasClass("donation-step-hidden") && step3.hasClass("donation-step-hidden")) {
+          step3.removeClass("donation-step-hidden");
+          step4.removeClass("donation-step-hidden");
+          btn.remove();
+        }
+      });
+    }
+  }
+});
